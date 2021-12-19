5c8ad7e83021e475541005d2f4f432dca83a2969
LAXMANREDDYAILENI <laxmanreddy.aileni97@gmail.com>
2021-12-19 15:05:51 -0800

    Updates

---
+++ precache-manifest.7f5ba0fd524370037e743e74866a0c83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a118b3b550f2d7427c44",
+    "url": "/portfolio/static/css/main.423ece12.chunk.css"
+  },
+  {
+    "revision": "a118b3b550f2d7427c44",
+    "url": "/portfolio/static/js/main.a118b3b5.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6fc9dff72bc34e7fbb908775b7c59255",
+    "url": "/portfolio/index.html"
+  }
+];
